a5f8e86ef216095d0da0d99ffbe41370f32a6dd8
Shubham <tshubhamucem@gmail.com>
2023-06-11 10:15:42 +0530

    testing

---
+++ kanban/src/Constant/constColor.js
@@ -0,0 +1,50 @@
+export const colors=[
+
+   {
+    color:"#9AC5F4"
+   },
+   {
+    color:"#C07F00"
+   },
+   {
+    color:"#443C68"
+   },
+   {
+    color:"#E41655"
+   },
+   {
+    color:"#A27B5C"
+   },
+   {
+    color:"#9E7540"
+   },
+   {
+    color:"#C6A9A3"
+   },
+   {
+    color:"#AAC8A7"
+   },
+   {
+    color:"#5F8D4E"
+   },
+   {
+    color:"#FCFDF2"
+   },
+   {
+    color:"#FF8787"
+   },
+   {
+    color:"#FF5403"
+   },
+   {
+    color:"#FFA36C"
+   },
+   {
+    color:"#018383"
+   },
+   {
+    color:"#A9ECA2"
+   },
+   
+
+];
---
+++ kanban/src/Constant/constImage.js
@@ -0,0 +1,46 @@
+
+export  const ImageUrl=[
+ 
+    {
+        
+        url:"https://cdn.wallpapersafari.com/97/60/6tRoMc.jpg"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1559666126-84f389727b9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1777&q=80"
+    },
+    
+    {
+        url:"https://images.pexels.com/photos/1903702/pexels-photo-1903702.jpeg?auto=compress&cs=tinysrgb&w=1600"
+    },
+    {
+        url:"https://img.freepik.com/free-photo/dramatic-white-clouds-blue-sky-from-airplane-window-view-colorful-sunset-cloudscape-background_90220-1208.jpg"
+    },
+    {
+        url:"https://static-cse.canva.com/blob/572026/removingbackgroundimages_Unsplash.jpeg"
+    },
+    {
+        url:" https://images.pexels.com/photos/545968/pexels-photo-545968.jpeg?auto=compress&cs=tinysrgb&w=1600"
+
+    },
+   
+    {
+        url:" https://images.pexels.com/photos/96627/pexels-photo-96627.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+
+    },
+    {
+        url:" https://images.pexels.com/photos/3680903/pexels-photo-3680903.jpeg?auto=compress&cs=tinysrgb&w=1600"
+
+    },
+    {
+        url:" https://images.pexels.com/photos/6923731/pexels-photo-6923731.jpeg?auto=compress&cs=tinysrgb&w=1600"
+
+    },
+    {
+        url:" https://images.pexels.com/photos/6897433/pexels-photo-6897433.jpeg?auto=compress&cs=tinysrgb&w=1600"
+
+    },
+    
+
+
+    
+]
